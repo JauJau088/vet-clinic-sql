3a8c4870cc1ce0bd3b420e853c5a0abc9868e30b
jaujau088 <jaujau088@gmail.com>
2022-07-05 15:32:05 +0200

    Add new data

--- schema.sql
+++ schema.sql
@@ -1,3 +1,4 @@
+--## Assignment 1
 CREATE TABLE animals (
     id INTEGER primary key GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(250),
@@ -6,3 +7,7 @@ CREATE TABLE animals (
     neutered BOOLEAN,
     weight_kg DECIMAL
 );
+
+--## Assignment 2
+ALTER TABLE animals
+ADD COLUMN species VARCHAR(250);
