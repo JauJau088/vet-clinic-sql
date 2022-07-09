b6401bfc665d256ba2e8f0c519251e9c5b849ff2
jaujau088 <jaujau088@gmail.com>
2022-07-09 11:58:13 +0200

    Add owners and species tables

--- schema.sql
+++ schema.sql
@@ -11,3 +11,15 @@ CREATE TABLE animals (
 --## Assignment 2
 ALTER TABLE animals
 ADD COLUMN species VARCHAR(250);
+
+--## Assignment 3
+CREATE TABLE owners (
+    id INTEGER primary key GENERATED ALWAYS AS IDENTITY,
+    full_name VARCHAR(250),
+    age INTEGER
+);
+
+CREATE TABLE species (
+    id INTEGER primary key GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(250)
+);
